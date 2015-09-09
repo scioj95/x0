4ef8bff300121a5147dd1c1983752bcae632422a
scioj95 <josephsciorilli@yahoo.com>
2015-09-09 16:29:03 -0400

    Update circles0.java
    changed screen size and location/size of the circle

--- circles0.java
+++ circles0.java
@@ -2,10 +2,10 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(350, 150);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 40,90, 45, 45 );       // Try changing the (x,y) and size.
 }
